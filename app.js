81b77ffbf2060feec1fe95b0a59448c4b58fd898
William Poon <willphk@outlook.com>
2023-04-30 00:51:19 -0400

    Fixed App.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ passport.use(facebookStrategry);
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/users", usersRouter);
-app.use("/friend-request", friendRequestRouter);
-app.use("/post", postRouter);
+app.use("/friend-requests", friendRequestRouter);
+app.use("/posts", postRouter);
 
 module.exports = app;
